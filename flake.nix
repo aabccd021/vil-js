e63e88e8d1dafcd7c4df52b17b459ec3355bfe4d
aabccd021 <aabccd021@gmail.com>
2025-01-30 17:55:01 +0700

    fix: update playwright import for browsers path

--- flake.nix
+++ flake.nix
@@ -124,7 +124,7 @@
 
       devShells.x86_64-linux.default = pkgs.mkShellNoCC {
         shellHook = ''
-          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright.browsers}
         '';
         buildInputs = [
           pkgs.nodejs
