c3bc9828fcbfc0bebf456836092c6cdd7c4deb7d
aabccd021 <aabccd021@gmail.com>
2025-01-30 17:56:39 +0700

    Update dependencies and configurations for Playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,13 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          ignoreDefaultArgs: ["--headless=old"],
+          args: ["--headless"],
+        },
+      },
     },
     {
       name: "firefox",
