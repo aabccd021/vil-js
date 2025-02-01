0bc7cf8dbdea66eb389822f1f0e8a370cfe31b16
aabccd021 <aabccd021@gmail.com>
2025-02-02 05:36:36 +0700

    feat: scroll items 8 and 18 into view

--- vil.test.ts
+++ vil.test.ts
@@ -87,6 +87,9 @@ test("bottom top", async ({ page }) => {
   await expect(items.first()).toHaveText("Item 0");
   await expect(items.last()).toHaveText("Item 7");
 
+  await page.getByText("Item 8").scrollIntoViewIfNeeded();
+  await page.getByText("Item 18").scrollIntoViewIfNeeded();
+
   await page.waitForTimeout(500);
   await page.mouse.wheel(0, 100);
   await scrollToBottom(scrollable);
