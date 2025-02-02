cab58e1587811ba8da48e54f061c457e04853484
aabccd021 <aabccd021@gmail.com>
2025-02-02 18:56:10 +0700

    checkpoint

--- stories/invoke-hook.js
+++
@@ -1,6 +0,0 @@
-var n = document.querySelectorAll("script");
-for (let o of Array.from(n)) {
-  if (o.type !== "module") continue;
-  let t = o.dataset.invokeHook;
-  t !== void 0 && import(o.src).then((e) => e[t]());
-}
