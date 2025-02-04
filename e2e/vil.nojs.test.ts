4a6d5f12c6c96cbde13790a85e9d6a96193306e6
aabccd021 <aabccd021@gmail.com>
2025-02-05 06:11:24 +0700

    feat(e2e): add test for single page with JS disabled

--- e2e/vil.nojs.test.ts
+++ e2e/vil.nojs.test.ts
@@ -1,8 +1,10 @@
 import { expect, test } from "@playwright/test";
+import { expectRendered } from "./testUtils";
 
 test.use({ javaScriptEnabled: false });
 
 test("Single page with js disabled", async ({ page }) => {
   await page.goto("/single-page.html");
+  await expectRendered(page, 0, 0, 4, 29);
   await expect(page).toHaveScreenshot("single-page-js-disabled.png");
 });
