205a86176a23be9d50434eaf71e7e5801357e856
aabccd021 <aabccd021@gmail.com>
2025-02-05 06:22:38 +0700

    Refactor e2e utils.ts for readability and reusability

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -24,3 +24,47 @@ export async function expectRendered(
     await expect(page.getByText(itemText(i))).not.toBeInViewport();
   }
 }
+
+export type Log = {
+  consoleMessages: string[];
+  pageerrors: Error[];
+};
+
+export const expectClickable = async (page: Page, log: Log, text: string): Promise<void> => {
+  expect(log.consoleMessages).toEqual([]);
+  log.consoleMessages.length = 0;
+
+  await expect(page.getByText(text)).toBeInViewport();
+  await page.getByText(text).click();
+  expect(log.consoleMessages).toEqual([`Clicked on ${text}`]);
+  log.consoleMessages.length = 0;
+};
+
+export const initLog = (page: Page): Log => {
+  const consoleMessages: string[] = [];
+  const pageerrors: Error[] = [];
+
+  page.on("console", (msg) => consoleMessages.push(msg.text()));
+  page.on("pageerror", (msg) => pageerrors.push(msg));
+
+  return { consoleMessages, pageerrors };
+};
+
+export const scroll = async (page: Page, pixels: number): Promise<void> => {
+  const scrollDeltaAbs = 100;
+  const scrollDelta = pixels > 0 ? scrollDeltaAbs : -scrollDeltaAbs;
+  const iteration = pixels / scrollDelta;
+  if (!Number.isInteger(iteration)) {
+    throw new Error(`pixels must be a multiple of ${scrollDelta}`);
+  }
+  for (let i = 0; i < iteration; i++) {
+    await page.mouse.wheel(0, scrollDelta);
+  }
+};
+
+export const expectPageErrorsEmpty = (log: Log): void => {
+  const pageErrors = log.pageerrors.filter(
+    (err) => err.message !== "ResizeObserver loop completed with undelivered notifications.",
+  );
+  expect(pageErrors).toEqual([]);
+};
